29db6d453fa691a695a3682806d3c0bf23dfd392
roniekun <roniebenitez01@gmail.com>
2023-10-04 21:08:56 +0800

    up

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,62 +1,125 @@
-import {useEffect, useRef} from 'react'
-import styles from './Portfolio.module.css'
-import Header from '../Header'
-import Footer from '../Footer'
-import PageWrapper from '../../PageWrapper'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './Portfolio.module.css';
+import Header from '../Header';
+import Footer from '../Footer';
+import PageWrapper from '../../PageWrapper';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-const Portfolio = ({isDesktop,setShowNavbar, isMediumScreen, isSmallScreen,showNavbar,isLoading}) => {
-  
+const Portfolio = ({
+  isDesktop,
+  setShowNavbar,
+  isMediumScreen,
+  isSmallScreen,
+  showNavbar,
+  isLoading,
+}) => {
+  const [activeItem, setActiveItem] = useState(0);
+  const [openGallery, setOpenGallery] = useState();
   const imageArray = [];
 
-  for (let id = 1; id <= 60; id++) {
+  for (let id = 1; id <= 59; id++) {
     imageArray.push({
-      src: `photos/${id}.jpg`
+      src: `photos/${id}.jpg`,
     });
   }
 
-    const imgRefs = imageArray.map(() => useRef(null));
-    const imgContainerRefs = imageArray.map(() => useRef(null));
+  const imgRefs = imageArray.map(() => useRef(null));
+  const imgContainerRefs = imageArray.map(() => useRef(null));
 
   useEffect(() => {
-       gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger);
 
-       imgRefs.forEach((imgRef, index) => {
-
-        gsap.to(imgRef.current, {
-          opacity: 1,
-          scrollTrigger: {
-            trigger: imgRef.current,
-            start: "top center",
-            end: "center center",
-          }
-        });
+    imgRefs.forEach((imgRef) => {
+      gsap.to(imgRef.current, {
+        opacity: 1,
+        scrollTrigger: {
+          trigger: imgRef.current,
+          start: 'top center',
+          end: 'center center',
+        },
       });
+    });
+  }, []);
 
-      }, []); // Run this effect only once
+  const handleClick = (index) => {
+    setActiveItem(index);
+    setOpenGallery(!openGallery);
+  };
+
   
-  return (
-   <>
-    <div className={styles.container}>
-          <Header isDesktop={isDesktop}
-              isMediumScreen={isMediumScreen}
-              isSmallScreen={isSmallScreen}
-              setShowNavbar={setShowNavbar}
-              setColor={'black'}
-              showNavbar={showNavbar}/>
-    <PageWrapper>
-    <div  className={styles.body}>
-     {imageArray.map((images, index)=>
-     (<div ref={imgContainerRefs[index]} key={index} className={styles.imageContainer}>
-        <img ref={imgRefs[index]} src={images.src}  alt="Larry Chen Photos" />
-        </div>))}
-    </div>
-    </PageWrapper>
-     <Footer/>
-    </div>
-    </>
-  )
-}
+  const handleExit = () => {
+    setOpenGallery(!openGallery);
+  };
 
-export default Portfolio
+  const handleNext = () => {
+  
+    if (activeItem === imageArray.length - 1) {
+      // If activeItem is at the last index, loop back to the first index (0).
+      setActiveItem(0);
+    } else {
+      setActiveItem((prev) => prev + 1);
+    }
+    console.log(activeItem);
+
+  };
+  
+  const handlePrevious = () => {
+    if (activeItem === 0) {
+      // If activeItem is at the first index, loop to the last index.
+      setActiveItem(imageArray.length - 1);
+    } else {
+      setActiveItem((prev) => prev - 1);
+    }
+    console.log(activeItem);
+
+  };
+  
+
+  return (
+    <>
+       {openGallery && (
+            <div className={styles.imageCarousel}>
+              <img  className={styles.activeImage}src={imageArray[activeItem].src} alt="Selected Image" />
+              <button className={styles.btnExit} onClick={handleExit}>X</button>
+              <div className={styles.btnContainer}>
+              <button className={styles.btnPrevious}  onClick={handlePrevious}>Previous</button>
+              <button className={styles.btnNext}onClick={handleNext}>Next</button>
+              </div>
+            </div>
+          )}
+
+      <div className={styles.container}>
+        <Header
+          isDesktop={isDesktop}
+          isMediumScreen={isMediumScreen}
+          isSmallScreen={isSmallScreen}
+          setShowNavbar={setShowNavbar}
+          setColor={'black'}
+          showNavbar={showNavbar}
+        />
+        <PageWrapper>
+          <div className={styles.body}>
+            {imageArray.map((images, index) => (
+              <div
+                ref={imgContainerRefs[index]}
+                key={index}
+                className={styles.imageContainer}
+              >
+                <img
+                  onClick={() => handleClick(index)}
+                  ref={imgRefs[index]}
+                  src={images.src}
+                  alt="Larry Chen Photos"
+                />
+              </div>
+            ))}
+          </div>
+        </PageWrapper>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Portfolio;
